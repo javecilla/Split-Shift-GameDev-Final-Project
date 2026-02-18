e875f1239028925832718bb8b9b5dfffac82f326
Jerome Avecilla <102340560+javecilla@users.noreply.github.com>
2026-02-18 20:18:12 +0800

    chore: added vscode editor configuration and setups

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 public class TestScript : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float movementSpeed = 10f;
+    public float movementSpeed = 15f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
