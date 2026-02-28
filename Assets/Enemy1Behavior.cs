36949094f376310cb13e57a13744cb1675548fe6
rensen <delacruzrensen@gmail.com>
2026-02-28 12:12:32 +0800

    Added Script for Enemy 1 Behavior

--- Assets/Enemy1Behavior.cs
+++ Assets/Enemy1Behavior.cs
@@ -5,7 +5,7 @@ public class Enemy1Behavior : MonoBehaviour
     public void NormalAttackDamage()
     {
         // need lng maaccess method na to para sa event frame sa animation
-    }
+        }
 
     public void ChargedAttackDamage()
     {
