028cabba5f170209f5532ceb96ccd6c06b405de6
rensen <delacruzrensen@gmail.com>
2026-02-28 11:32:39 +0800

    Feat: Set Up Animator Blend Tree for Enemy 1 - Dog

---
+++ Assets/Enemy1Behavior.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Enemy1Behavior : MonoBehaviour
+{
+    public void NormalAttackDamage()
+    {
+        // need lng maaccess method na to para sa event frame sa animation
+    }
+
+    public void ChargedAttackDamage()
+    {
+        // need lng maaccess method na to para sa event frame sa animation
+    }
+
+
+}
